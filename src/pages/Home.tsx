4b3fbb4d2b19a9b040b9638e7044bdc21f3d392c
Deniz <auo@tuta.io>
2025-10-10 15:02:39 +0200

    Correct home card link. Added more info button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,8 +14,8 @@ export default function Home() {
             <p className="align-text">Find the variety you have been looking for.</p>
             <p className="align-text">Super organic pure herbs and plants.</p>
             <p className="align-text">See more in our product page.</p>
-            <Link to={`/products/${herbsData[randomNum].name.toLowerCase()}`} >
                 <div className="promo-home">
+            <Link to={`/products/${herbsData[randomNum].name.toLowerCase()}`} >
                     <HerbDetailContainer
                         imgSrc={`/images/${herbsData[randomNum].image}`}
                         name={herbsData[randomNum].name}
@@ -23,19 +23,23 @@ export default function Home() {
                         isCulinary={herbsData[randomNum].isCulinary}
                     >
                         <p>{herbsData[randomNum].description}</p>
+                        <p className="button-p">Click for more info</p>
                     </HerbDetailContainer>
+                    </Link>
                    
-                    <HerbDetailContainer
-                        imgSrc={`/images/${herbsData[randomTwo].image}`}
-                        name={herbsData[randomTwo].name}
-                        bgColor="#a3581bff"
-                        price={herbsData[randomTwo].price}
-                        isCulinary={herbsData[randomTwo].isCulinary}
-                    >
-                        <p>{herbsData[randomTwo].description}</p>
-                    </HerbDetailContainer>
+                    <Link to={`/products/${herbsData[randomTwo].name.toLowerCase()}`} >
+                        <HerbDetailContainer
+                            imgSrc={`/images/${herbsData[randomTwo].image}`}
+                            name={herbsData[randomTwo].name}
+                            bgColor="#a3581bff"
+                            price={herbsData[randomTwo].price}
+                            isCulinary={herbsData[randomTwo].isCulinary}
+                        >
+                            <p>{herbsData[randomTwo].description}</p>
+                            <p className="button-p">Click for more info</p>
+                        </HerbDetailContainer>
+                    </Link>
                 </div>
-            </Link>
             <h3 className="align-text">Go ahead and check a super organic new hype plants in the shop.</h3>
             <h4 className="align-text">Click the link below</h4>
             <Link to="/products" className="to-btn">Products</Link>
