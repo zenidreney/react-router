3f034b9fdf91a34d608f163f60fcfa14be8e5ea7
Deniz <auo@tuta.io>
2025-12-04 16:40:27 +0100

    More code refactor

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,73 +1,75 @@
-import { useContext } from "react"
-import { CartContext } from "../context/CartContext"
+import { useContext } from "react";
+import type { CartItem } from "../context/CartContext";
+import { CartContext } from "../context/CartContext";
 import type { Herb } from "../data";
-import type { CartItem } from "../context/CartContext"
-
 
 function useCart() {
-    const context = useContext(CartContext)
-    if (!context) {
-        throw new Error("Please use useCart in CartContextProvider")
-    }
-    const {
-        cartItems, setCartItems,
-        formData, setFormData,
-        completedOrder, setCompletedOrder } = context
+	const context = useContext(CartContext);
+	if (!context) {
+		throw new Error("Please use useCart in CartContextProvider");
+	}
+	const {
+		cartItems,
+		setCartItems,
+		formData,
+		setFormData,
+		completedOrder,
+		setCompletedOrder,
+	} = context;
 
-    function addToCart(herb: Herb) {
+	function addToCart(herb: Herb) {
+		if (!herb) {
+			return;
+		}
+		setCartItems((prev) => {
+			const existingCartItem = prev.find((item) => item.name === herb.name);
 
-        if (!herb) {
-            return
-        }
-        setCartItems(prev => {
+			if (existingCartItem) {
+				return prev.map((item) => {
+					return item.name === herb.name
+						? { ...item, quantity: item.quantity + 1 }
+						: item;
+				});
+			} else return herb ? [...prev, { ...herb, quantity: 1 }] : prev;
+		});
+	}
 
-            const existingCartItem = prev.find(item => item.name === herb.name)
+	function removeFromCart(herb: CartItem) {
+		if (!herb) {
+			return;
+		}
 
-            if (existingCartItem) {
-                return prev.map(item => {
-                    return item.name === herb.name
-                        ? { ...item, quantity: item.quantity + 1 }
-                        : item
-                })
-            } else return herb ? [...prev, { ...herb, quantity: 1 }] : prev
-        })
-    }
+		setCartItems((prev) => {
+			const existingCartItem = prev.find((item) => item.name === herb.name);
 
-    function removeFromCart(herb: CartItem) {
+			if (!existingCartItem) {
+				return prev;
+			}
 
-        if (!herb) {
-            return
-        }
+			if (existingCartItem.quantity > 1) {
+				return prev.map((item) => {
+					return item.name === herb.name
+						? { ...item, quantity: item.quantity - 1 }
+						: item;
+				});
+			} else {
+				return prev.filter((item) => {
+					return item.name !== herb.name;
+				});
+			}
+		});
+	}
 
-        setCartItems(prev => {
-            const existingCartItem = prev.find(item => item.name === herb.name)
-
-            if (!existingCartItem) {
-                return prev
-            }
-
-            if (existingCartItem.quantity > 1) {
-                return prev.map(item => {
-                    return item.name === herb.name ?
-                        { ...item, quantity: item.quantity - 1 } :
-                        item
-                })
-            } else {
-                return prev.filter(item => {
-                    return item.name !== herb.name
-                })
-            }
-        })
-
-    }
-
-
-    return {
-        cartItems, setCartItems,
-        formData, setFormData,
-        completedOrder, setCompletedOrder,
-        addToCart, removeFromCart,
-    }
+	return {
+		cartItems,
+		setCartItems,
+		formData,
+		setFormData,
+		completedOrder,
+		setCompletedOrder,
+		addToCart,
+		removeFromCart,
+	};
 }
 
-export { useCart }
+export { useCart };
